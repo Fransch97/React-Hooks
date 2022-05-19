d7c2ddb52658a8b4fb6e77c380a554dca33be84f
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-19 22:05:52 +0200

    hooks

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,20 @@
 import "../css/main.css"
+import {useState} from "react"
 export default function Main(main){
-    
+    const [valore, setValue] = useState(0)
+    function incremento(){
+        setValue(valore + 1)
+    }
+    function decremento(){
+        setValue(valore - 1)
+    }
     return(
         <main>
             <h1>{main.main}</h1>
+            <p>Sperimentiamo gli hooks</p>
+            <button onClick={()=>{incremento()}}>Inscrementa</button>
+            <button onClick={()=>{decremento()}}>Decrementa</button>
+            <p>{valore}</p>
         </main>
     )
 }
